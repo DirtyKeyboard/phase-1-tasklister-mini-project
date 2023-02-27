40d8f615c8de89275f234b742a60dfc8e1fbb1bc
Andrew Hawileh <andrewh829@gmail.com>
2023-02-26 20:54:07 -0600

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addToList(document.getElementById('new-task-description').value)
+    form.reset();
+  })
 });
+
+
+function addToList(val)
+{
+  const listContainer = document.getElementById('tasks')
+  const listItem = document.createElement('li')
+  const del = document.createElement('button')
+  del.textContent = '✖'
+  listItem.textContent = val+"   ";
+  del.addEventListener('click', (e) => {
+    e.target.parentNode.remove()
+  })
+  listItem.appendChild(del)
+  listContainer.appendChild(listItem);
+}
